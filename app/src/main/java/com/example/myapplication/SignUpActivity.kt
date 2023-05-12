1f5fc30a779725a6117a1f6ea7fcd062caea8200
Dulajh <it21386336@my.sliit.lk>
2023-05-12 07:57:51 +0530

    SmartSpend MAD assignment all functions integrate

--- app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -6,11 +6,16 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+// ... imports
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,7 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance().reference
 
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
@@ -33,12 +39,20 @@ class SignUpActivity : AppCompatActivity() {
                 if (password == (confirmPassword)) {
 
                     firebaseAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener {
-                            if (it.isSuccessful) {
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val user = firebaseAuth.currentUser
+                                val userId = user?.uid ?: ""
+                                val email = user?.email ?: ""
+
+                                val userData = User(userId, email) // create a User object with the new user's data
+                                firebaseDatabase.child("users").child(userId).setValue(userData) // save the user data to the Realtime Database
+
                                 val intent = Intent(this, SignInActivity::class.java)
                                 startActivity(intent)
+                                Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show()
                             } else {
-                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT)
+                                Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT)
                                     .show()
                             }
                         }
@@ -47,8 +61,61 @@ class SignUpActivity : AppCompatActivity() {
                     Toast.makeText(this, "Password not matching", Toast.LENGTH_SHORT).show()
                 }
             } else {
-                Toast.makeText(this, "required to fill all the columns", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Required to fill all the columns", Toast.LENGTH_SHORT).show()
             }
         }
     }
 }
+
+// define a User data class to represent the user data you want to save
+data class User(val userId: String = "", val email: String = "")
+
+
+
+
+//class SignUpActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivitySignUpBinding
+//    private lateinit var firebaseAuth: FirebaseAuth
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivitySignUpBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        firebaseAuth = FirebaseAuth.getInstance()
+//
+//        binding.textView.setOnClickListener {
+//            val intent = Intent(this, SignInActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        binding.button.setOnClickListener {
+//            val email = binding.emailEt.text.toString()
+//            val password = binding.passET.text.toString()
+//            val confirmPassword = binding.confirmPassEt.text.toString()
+//
+//            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+//                if (password == (confirmPassword)) {
+//
+//                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener {
+//                            if (it.isSuccessful) {
+//                                val intent = Intent(this, SignInActivity::class.java)
+//                                startActivity(intent)
+//                                Toast.makeText(this, "Successfull", Toast.LENGTH_SHORT).show()
+//                            } else {
+//                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT)
+//                                    .show()
+//                            }
+//                        }
+//
+//                } else {
+//                    Toast.makeText(this, "Password not matching", Toast.LENGTH_SHORT).show()
+//                }
+//            } else {
+//                Toast.makeText(this, "required to fill all the columns", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
+//}
